cbb43f2ee7aecf0a49f30d6abcb05bf647e64dbf
changchang cao <34830720+caochch@users.noreply.github.com>
2021-07-24 14:59:37 +0800

    Add files via upload

---
+++ 7.mapping_and_pairs/find_all_pairs/work.sh
@@ -0,0 +1,26 @@
+#process mapped results
+samtools view -@ 10 -b -S -o patientA_rep1_read1_toGenomeAligned.out.bam patientA_rep1_read1_toGenomeAligned.out.sam 
+samtools view -@ 10 -b -S -o patientA_rep1_read2_toGenomeAligned.out.bam patientA_rep1_read2_toGenomeAligned.out.sam 
+samtools sort -@ 10 patientA_rep1_read1_toGenomeAligned.out.bam patientA_rep1_read1_toGenomeAligned.out.sort 
+samtools sort -@ 10 patientA_rep1_read2_toGenomeAligned.out.bam patientA_rep1_read2_toGenomeAligned.out.sort 
+
+samtools view -@ 10 -h -q 30 -F 256 -b -o patientA_rep1_read1_toGenomeAligned.out.sort.uniq.bam patientA_rep1_read1_toGenomeAligned.out.sort.bam 
+samtools view -@ 10 -h -q 30 -F 256 -b -o patientA_rep1_read2_toGenomeAligned.out.sort.uniq.bam patientA_rep1_read2_toGenomeAligned.out.sort.bam
+samtools view -h -o patientA_rep1_read1_toGenomeAligned.out.sort.uniq.sam patientA_rep1_read1_toGenomeAligned.out.sort.uniq.bam 
+samtools view -h -o patientA_rep1_read2_toGenomeAligned.out.sort.uniq.sam patientA_rep1_read2_toGenomeAligned.out.sort.uniq.bam
+perl precess_Chimeric_sam.pl patientA_rep1_read1_toGenomeChimeric.out.sam > patientA_rep1_read1_toGenomeChimeric.out.processed.sam 
+perl precess_Chimeric_sam.pl patientA_rep1_read2_toGenomeChimeric.out.sam > patientA_rep1_read2_toGenomeChimeric.out.processed.sam
+
+#find pairs
+perl obtain_pairs_from_pair.pl patientA_rep1_read1_toGenomeAligned.out.sort.uniq.sam patientA_rep1_read2_toGenomeAligned.out.sort.uniq.sam
+samtools view -@ 10 -b -S -o interaction_from_pair_mapped_reads_1.bam interaction_from_pair_mapped_reads_1.sam
+samtools view -@ 10 -b -S -o interaction_from_pair_mapped_reads_2.bam interaction_from_pair_mapped_reads_2.sam
+samtools sort -n -@ 10 interaction_from_pair_mapped_reads_1.bam interaction_from_pair_mapped_reads_1.sort
+samtools sort -n -@ 10 interaction_from_pair_mapped_reads_2.bam interaction_from_pair_mapped_reads_2.sort
+samtools view -h -o interaction_from_pair_mapped_reads_1.sort.sam interaction_from_pair_mapped_reads_1.sort.bam
+samtools view -h -o interaction_from_pair_mapped_reads_2.sort.sam interaction_from_pair_mapped_reads_2.sort.bam
+
+perl obtain_pairs_from_gapped_reads.pl patientA_rep1_read1_toGenomeAligned.out.sort.uniq.sam patientA_rep1_read2_toGenomeAligned.out.sort.uniq.sam patientA_rep1_read1_toGenomeChimeric.out.processed.sam patientA_rep1_read2_toGenomeChimeric.out.processed.sam interaction_from_gapped_reads.sam
+
+perl merge_interaction.pl num_of_interactions_from_part.list interaction_from_pair_mapped_reads_1.sort.sam interaction_from_pair_mapped_reads_2.sort.sam interaction_from_gapped_reads.sam patientA_rep1_read1_toGenomeChimeric.out.processed.sam patientA_rep1_read2_toGenomeChimeric.out.processed.sam patientA_rep1_interaction.sam
+perl count_link_for_each_kind.pl interaction_from_pair_mapped_reads_1.sort.sam interaction_from_pair_mapped_reads_2.sort.sam interaction_from_gapped_reads.sam patientA_rep1_read1_toGenomeChimeric.out.processed.sam patientA_rep1_read2_toGenomeChimeric.out.processed.sam > num_of_interactions.list
---
+++ 7.mapping_and_pairs/main_pipeline.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+my @samples=(
+"5.patientA_rep1",
+"6.patientA_rep2",
+"7.patientB_rep1",
+"8.patientB_rep2",
+"patient-1",
+"patient-2"
+);
+
+my $STAR_index_dir="/mnt/pub/work/caochch/project_My/project033_RIC_Covid19_withHost_WIV/database/Pangenome_human";
+my $genome_ref_fasta="/mnt/pub/work/caochch/project_My/project033_RIC_Covid19_withHost_WIV/database/Pangenome_human/human_virus.pan.fa";
+
+open(TSH,">run.sh") || die;
+foreach my $d (@samples){
+	my $prefix=$d;
+	$prefix=~s/^\d+\.//;
+
+	#`mkdir ./$d`;
+        my $in_read_1="../../5.split_rRNA_and_otherReads/$d/z1.second_round_byBWA/read1_torRNA_Unmapped_really.FutherByBwa.fq";
+        my $in_read_2="../../5.split_rRNA_and_otherReads/$d/z1.second_round_byBWA/read2_torRNA_Unmapped_really.FutherByBwa.fq";
+        #`ln -s $in_read_1 ./$d`;
+        #`ln -s $in_read_2 ./$d`;
+
+	#print TSH "STAR --runMode alignReads --genomeDir $STAR_index_dir --readFilesIn ./$d/read1_torRNA_Unmapped_really.FutherByBwa.fq --outFileNamePrefix ./$d/$prefix","_read1_toGenome --outReadsUnmapped Fastx --outFilterMultimapNmax 100 --outSAMattributes All --alignIntronMin 1 --scoreGapNoncan -4 --scoreGapATAC -4 --chimSegmentMin 15 --chimJunctionOverhangMin 15 --limitOutSJcollapsed 10000000 --limitIObufferSize 1500000000 --runThreadN 16 --alignSJoverhangMin 15 --alignSJDBoverhangMin 10 --alignSJstitchMismatchNmax 5 -1 5 5 --outFilterMatchNminOverLread 0.5 --outFilterScoreMinOverLread 0.5 &\n";
+	#print TSH "STAR --runMode alignReads --genomeDir $STAR_index_dir --readFilesIn ./$d/read2_torRNA_Unmapped_really.FutherByBwa.fq --outFileNamePrefix ./$d/$prefix","_read2_toGenome --outReadsUnmapped Fastx --outFilterMultimapNmax 100 --outSAMattributes All --alignIntronMin 1 --scoreGapNoncan -4 --scoreGapATAC -4 --chimSegmentMin 15 --chimJunctionOverhangMin 15 --limitOutSJcollapsed 10000000 --limitIObufferSize 1500000000 --runThreadN 16 --alignSJoverhangMin 15 --alignSJDBoverhangMin 10 --alignSJstitchMismatchNmax 5 -1 5 5 --outFilterMatchNminOverLread 0.5 --outFilterScoreMinOverLread 0.5\n";
+
+	#print TSH "perl /media/ibm_disk/work/caochch/project_My/RICpipeV2_20210713/7.mapping_and_pairs/select_not_fully_aligned.pl ./$d/read1_torRNA_Unmapped_really.FutherByBwa.fq ./$d/$prefix","_read1_toGenomeAligned.out.sam ./$d/$prefix","_read1_toGenomeChimeric.out.sam > ./$d/$prefix","_read1_toGenomeUnmapped_really.fq &\n";
+	#print TSH "perl /media/ibm_disk/work/caochch/project_My/RICpipeV2_20210713/7.mapping_and_pairs/select_not_fully_aligned.pl ./$d/read2_torRNA_Unmapped_really.FutherByBwa.fq ./$d/$prefix","_read2_toGenomeAligned.out.sam ./$d/$prefix","_read2_toGenomeChimeric.out.sam > ./$d/$prefix","_read2_toGenomeUnmapped_really.fq\n";
+
+	#`mkdir ./$d/z1.second_round_byBWA`;
+	#print TSH "/media/ibm_disk/work/caochch/project_My/RICpipeV2_20210713/bwa-master/bwa mem -t 16 -k 12 -T 15 -o ./$d/z1.second_round_byBWA/read1_futher_by_bwa.sam $genome_ref_fasta ./$d/$prefix","_read1_toGenomeUnmapped_really.fq\n";
+	#print TSH "/media/ibm_disk/work/caochch/project_My/RICpipeV2_20210713/bwa-master/bwa mem -t 16 -k 12 -T 15 -o ./$d/z1.second_round_byBWA/read2_futher_by_bwa.sam $genome_ref_fasta ./$d/$prefix","_read2_toGenomeUnmapped_really.fq\n";
+
+	#print TSH "perl /media/ibm_disk/work/caochch/project_My/RICpipeV2_20210713/7.mapping_and_pairs/collect_chimeric_ligation_from_sam.pl ./$d/z1.second_round_byBWA/read1_futher_by_bwa.sam 1 ./$d/z1.second_round_byBWA > ./$d/z1.second_round_byBWA/out1.read1.chimeric.sam\n";
+	#print TSH "perl /media/ibm_disk/work/caochch/project_My/RICpipeV2_20210713/7.mapping_and_pairs/collect_chimeric_ligation_from_sam.pl ./$d/z1.second_round_byBWA/read2_futher_by_bwa.sam 2 ./$d/z1.second_round_byBWA > ./$d/z1.second_round_byBWA/out1.read2.chimeric.sam \n";
+
+	#print TSH "perl /media/ibm_disk/work/caochch/project_My/RICpipeV2_20210713/7.mapping_and_pairs/get_final_unmapped_reads.pl ./$d/$prefix","_read1_toGenomeUnmapped_really.fq ./$d/z1.second_round_byBWA/unmapped.1.readID.list > ./$d/z1.second_round_byBWA/read1_torRNA_Unmapped_really.FutherByBwa.fq\n";
+	#print TSH "perl /media/ibm_disk/work/caochch/project_My/RICpipeV2_20210713/7.mapping_and_pairs/get_final_unmapped_reads.pl ./$d/$prefix","_read2_toGenomeUnmapped_really.fq ./$d/z1.second_round_byBWA/unmapped.2.readID.list > ./$d/z1.second_round_byBWA/read2_torRNA_Unmapped_really.FutherByBwa.fq\n";
+
+	#you can stop if you do not care about the interaction in this given reference
+	
+	#run find_all_pairs first
+
+	#`mkdir ./$d/z2.update_interaction_sam`;
+	#print TSH "perl /media/ibm_disk/work/caochch/project_My/RICpipeV2_20210713/7.mapping_and_pairs/supp_bwaSam_and_update.pl ./$d/find_all_pairs/$prefix","_interaction.sam ./$d/find_all_pairs/num_of_interactions_from_part.list ./$d/find_all_pairs/num_of_interactions.list ./$d/z1.second_round_byBWA/out1.read1.chimeric.sam ./$d/z1.second_round_byBWA/out1.read2.chimeric.sam ./$d/z2.update_interaction_sam > ./$d/z2.update_interaction_sam/mergeBwa.$prefix","_interaction.sam\n";	
+	`mkdir ./$d/z3.update_alignPair`;
+	print TSH "perl /media/ibm_disk/work/caochch/project_My/RICpipeV2_20210713/7.mapping_and_pairs/replace_AlignPair_by_ChimericFragment.pl ./$d/z2.update_interaction_sam/mergeBwa.$prefix","_interaction.sam ./$d/find_all_pairs/$prefix","_read1_toGenomeChimeric.out.processed.sam ./$d/find_all_pairs/$prefix","_read2_toGenomeChimeric.out.processed.sam ./$d/z1.second_round_byBWA/out1.read1.chimeric.sam ./$d/z1.second_round_byBWA/out1.read2.chimeric.sam > ./$d/z3.update_alignPair/update.mergeBwa.$prefix","_interaction.sam\n";
+
+ 
+
+}
